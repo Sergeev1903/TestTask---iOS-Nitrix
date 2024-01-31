4887bc7932039365b922b89fc6b244d1c4101eb7
Artem Sergeev <sergeev190391@gmail.com>
2024-01-31 13:47:48 +0200

    Initial Commit

---
+++ TestTask - iOS Nitrix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask - iOS Nitrix
+//
+//  Created by Артем Сергеев on 31.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
